845599e11677efb5043e6581a38006fe41092b32
v-lixyua <v-lixyua@microsoft.com>
2015-11-12 16:28:47 +0800

    rename

---
+++ TTS_np_plugin/TTS_np_plugin/Main.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using NppPluginNET;
+
+namespace NppSDPlugin
+{
+    struct ShortCutConfig
+    {
+        public string ConfigKeyName;
+        public Keys ShortKey;
+        public bool IsCtrl;
+        public bool IsAlt;
+        public bool IsShift;
+    }
+    class Main
+    {
+        #region " Fields "
+        internal const string PluginName = "TTS_SD_Command";
+        internal const string PluginRootPathKey = "BranchRoot";
+        static string iniFilePath = null;
+        static string BanchRootPath = null;
+        static frmSetRoot frmSetRoot = null;
+        static int idMyDlg = -1;
+        static Bitmap tbBmp = Properties.Resources.star;
+        static Bitmap tbBmp_tbTab = Properties.Resources.star_bmp;
+        //static Icon tbIcon = null;
+
+        static ShortCutConfig sdEditConfig = new ShortCutConfig { ConfigKeyName = "SD edit Shortcut", ShortKey = Keys.S, IsCtrl = true, IsAlt = true };
+        static ShortCutConfig sdRevertConfig = new ShortCutConfig { ConfigKeyName = "SD revert Shortcut", ShortKey = Keys.None };
+        #endregion
+
+        #region " StartUp/CleanUp "
+        internal static void CommandMenuInit()
+        {
+            StringBuilder sbIniFilePath = new StringBuilder(Win32.MAX_PATH);
+            Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_GETPLUGINSCONFIGDIR, Win32.MAX_PATH, sbIniFilePath);
+            iniFilePath = sbIniFilePath.ToString();
+            if (!Directory.Exists(iniFilePath)) Directory.CreateDirectory(iniFilePath);
+            iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
+
+            // get branch root path
+            StringBuilder sbTemp = new StringBuilder(Win32.MAX_PATH);
+            Win32.GetPrivateProfileString(PluginName, PluginRootPathKey, "", sbTemp, Win32.MAX_PATH, iniFilePath);
+            BanchRootPath = sbTemp.ToString();
+            SetShortCut(ref sdEditConfig);
+            SetShortCut(ref sdRevertConfig);
+
+            PluginBase.SetCommand(0, "sd edit", SDEdit, new ShortcutKey(sdEditConfig.IsCtrl, sdEditConfig.IsAlt, sdEditConfig.IsShift, sdEditConfig.ShortKey));
+            PluginBase.SetCommand(1, "sd revert", SDRevert, new ShortcutKey(sdRevertConfig.IsCtrl, sdRevertConfig.IsAlt, sdRevertConfig.IsShift, sdRevertConfig.ShortKey));
+            PluginBase.SetCommand(3, "set root path", SetBranchRoot);
+            idMyDlg = 1;
+        }
+        internal static void SetToolBarIcon()
+        {
+            toolbarIcons tbIcons = new toolbarIcons();
+            tbIcons.hToolbarBmp = tbBmp.GetHbitmap();
+            IntPtr pTbIcons = Marshal.AllocHGlobal(Marshal.SizeOf(tbIcons));
+            Marshal.StructureToPtr(tbIcons, pTbIcons, false);
+            Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_ADDTOOLBARICON, PluginBase._funcItems.Items[idMyDlg]._cmdID, pTbIcons);
+            Marshal.FreeHGlobal(pTbIcons);
+        }
+        internal static void PluginCleanUp()
+        {
+            Win32.WritePrivateProfileString(PluginName, PluginRootPathKey, BanchRootPath, iniFilePath);
+        }
+        #endregion
+
+        #region " Menu functions "
+
+        /// <summary>
+        /// Set shortcut config
+        /// </summary>
+        /// <param name="config">config object</param>
+        private static void SetShortCut(ref ShortCutConfig config)
+        {
+            StringBuilder sbTemp = new StringBuilder(10);
+
+            try
+            {
+                Win32.GetPrivateProfileString(config.ConfigKeyName, "ShortKey", "", sbTemp, Win32.MAX_PATH, iniFilePath);
+                if (sbTemp.Length > 0)
+                {
+                    config.ShortKey = (Keys)Enum.Parse(typeof(Keys), sbTemp.ToString());
+                }
+                else
+                {
+                    if (config.ShortKey != Keys.None)
+                    {
+                        Win32.WritePrivateProfileString(config.ConfigKeyName, "ShortKey", config.ShortKey.ToString(), iniFilePath);
+                    }
+                }
+
+                Win32.GetPrivateProfileString(config.ConfigKeyName, "IsCtrl", "", sbTemp, Win32.MAX_PATH, iniFilePath);
+                if (sbTemp.Length > 0)
+                {
+                    config.IsCtrl = (int)Enum.Parse(typeof(int), sbTemp.ToString()) != 0;
+                }
+                else
+                {
+                    Win32.WritePrivateProfileString(config.ConfigKeyName, "IsCtrl", config.IsCtrl ? "" : "0", iniFilePath);
+                }
+
+                Win32.GetPrivateProfileString(config.ConfigKeyName, "IsAlt", "", sbTemp, Win32.MAX_PATH, iniFilePath);
+                if (sbTemp.Length > 0)
+                {
+                    config.IsAlt = (int)Enum.Parse(typeof(int), sbTemp.ToString()) != 0;
+                }
+                else
+                {
+                    Win32.WritePrivateProfileString(config.ConfigKeyName, "IsAlt", config.IsAlt ? "1" : "0", iniFilePath);
+                }
+
+                Win32.GetPrivateProfileString(config.ConfigKeyName, "IsShift", "", sbTemp, Win32.MAX_PATH, iniFilePath);
+                if (sbTemp.Length > 0)
+                {
+                    config.IsShift = (int)Enum.Parse(typeof(int), sbTemp.ToString()) != 0;
+                }
+                else
+                {
+                    Win32.WritePrivateProfileString(config.ConfigKeyName, "IsShift", config.IsShift ? "1" : "0", iniFilePath);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Promopt user set the branch root path
+        /// </summary>
+        /// <param name="initPath">init branch root path</param>
+        /// <param name="forceSet">force to reset path</param>
+        /// <returns>success or not</returns>
+        private static bool PromoptSelectBranchRoot(string initPath, bool forceSet = false)
+        {
+            if (forceSet || (String.IsNullOrEmpty(BanchRootPath)
+                   || !Directory.Exists(BanchRootPath)))
+            {
+                if (frmSetRoot == null)
+                {
+                    frmSetRoot = new frmSetRoot(initPath);
+                }
+
+                if (frmSetRoot.ShowDialog() == DialogResult.OK)
+                {
+                    Win32.WritePrivateProfileString(PluginName, PluginRootPathKey, frmSetRoot.SelectedPath, iniFilePath);
+                    BanchRootPath = frmSetRoot.SelectedPath;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// sd edit file
+        /// </summary>
+        private static void SDEdit()
+        {
+            if (PromoptSelectBranchRoot(BanchRootPath))
+            {
+                Util.SdCheckoutFile(BanchRootPath, GetCurrentFilePath());
+            }
+        }
+
+        /// <summary>
+        /// sd revert file
+        /// </summary>
+        private static void SDRevert()
+        {
+            if (PromoptSelectBranchRoot(BanchRootPath))
+            {
+                Util.SdRevertFile(BanchRootPath, GetCurrentFilePath());
+            }
+        }
+
+        /// <summary>
+        /// Set branch root path
+        /// </summary>
+        private static void SetBranchRoot()
+        {
+            PromoptSelectBranchRoot(BanchRootPath, true);
+        }
+
+        /// <summary>
+        /// Get current opened file path
+        /// </summary>
+        /// <returns></returns>
+        private static string GetCurrentFilePath()
+        {
+            // get current file path
+            StringBuilder sbFilePath = new StringBuilder(Win32.MAX_PATH);
+            Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_GETFULLCURRENTPATH, 0, sbFilePath);
+
+            return sbFilePath.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ TTS_np_plugin/TTS_np_plugin/Util.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace NppSDPlugin
+{
+    public static class Util
+    {
+        private const string SDPath = @"tools\coretools\sd.exe";
+        
+        /// <summary>
+        /// sd edit file
+        /// </summary>
+        /// <param name="filePath">file path</param>
+        public static void SdCheckoutFile(string branchRootPath, string filePath)
+        {
+            string message;
+            if (filePath.IndexOf(branchRootPath) != 0)
+            {
+                MessageBox.Show("Cannot sd edit file not in branch!");
+                return;
+            }
+
+            string sdToolPath = Path.Combine(branchRootPath, SDPath);
+
+            try
+            {
+                Int32 sdExitCode = CommandLine.RunCommand(sdToolPath, string.Format("edit {0}", filePath), true);
+
+                if (sdExitCode == 0)
+                {
+                    message = string.Format("Checked out file: {0}", filePath);
+                }
+                else
+                {
+                    message = string.Format("Failed to check out file: {0}", filePath);
+                }
+            }
+            catch (Exception e)
+            {
+                message = string.Format("{0} Failed to check out file: {1}", e.Message, filePath);
+                MessageBox.Show(message);
+            }
+        }
+
+        /// <summary>
+        /// sd revert file
+        /// </summary>
+        /// <param name="branchRootPath"></param>
+        /// <param name="filePath"></param>
+        public static void SdRevertFile(string branchRootPath, string filePath)
+        {
+            string message;
+            if (filePath.IndexOf(branchRootPath) != 0)
+            {
+                MessageBox.Show("Cannot sd revert file not in branch!");
+                return;
+            }
+
+            string sdToolPath = Path.Combine(branchRootPath, SDPath);
+
+            try
+            {
+                Int32 sdExitCode = CommandLine.RunCommand(sdToolPath, string.Format("revert {0}", filePath), true);
+
+                if (sdExitCode == 0 )
+                {
+                    message = string.Format("Reverted unchanged file: {0}", filePath);
+                }
+                else
+                {
+                    message = string.Empty;
+                }
+            }
+            catch (Exception e)
+            {
+                message = string.Format("{0} Failed to revert unchanged file: {1}.", e.Message, filePath);
+                MessageBox.Show(message);
+            }
+        }
+    }
+
+    public static class CommandLine
+    {        
+        /// <summary>
+        /// Lock object to protect Process.Start.
+        /// </summary>
+        private static object _processStartLock = new object();
+
+        private static Encoding defaultOutputEncoding = Encoding.UTF8;
+        private static Encoding defaultErrorEncoding = Encoding.UTF8;
+
+        public static int RunCommand(string command, string arguments, bool waitDone)
+        {
+            if (string.IsNullOrEmpty(command))
+            {
+                throw new ArgumentNullException("command");
+            }
+
+            if (string.IsNullOrEmpty(arguments))
+            {
+                arguments = string.Empty;
+            }
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = command;
+            psi.Arguments = arguments;
+            psi.WorkingDirectory = Path.GetDirectoryName(command);
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = false;
+            psi.RedirectStandardError = false;
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process process = new Process();
+            process.StartInfo = psi;
+            process.Start();
+
+            if (waitDone)
+            {
+                process.WaitForExit();
+            }
+            return process.ExitCode;
+        }
+
+ 
+    }
+}
